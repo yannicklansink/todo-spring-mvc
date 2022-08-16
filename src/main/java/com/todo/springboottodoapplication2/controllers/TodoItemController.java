6104275b34f3c99872e6c9e2bc0f3e95265c72e6
Yannick Lansink <yannick.lansink@live.nl>
2022-08-16 16:10:24 +0200

    update and delete forms

--- src/main/java/com/todo/springboottodoapplication2/controllers/TodoItemController.java
+++ src/main/java/com/todo/springboottodoapplication2/controllers/TodoItemController.java
@@ -1,12 +1,21 @@
 package com.todo.springboottodoapplication2.controllers;
 
+import java.time.Instant;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.todo.springboottodoapplication2.models.TodoItem;
 import com.todo.springboottodoapplication2.repositories.TodoItemRepository;
 
 @Controller
@@ -25,5 +34,16 @@ public class TodoItemController {
 		return modelAndView;
 	}
 	
+	@PostMapping("/todo/{id}")
+	public String updateTodoItem(@PathVariable("id") long id, @Valid TodoItem todoItem, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+	}
 	
 }
